c3e7bb1786b9e43864aac54b2e931045846c0b4e
Misato Fujihara <fujiharaionn@gmail.com>
2017-08-22 15:43:24 +0900

    aboutページの追加

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'sinatra'
 require 'sinatra/reloader'
 # htmlファイルを呼び出す
@@ -9,5 +10,14 @@ get '/:name' do |n|
     @name=n
     # 遠隔的に操作
     @title="main_index"
+    @content="main content"
     erb :index2
+end
+
+get '/about' do 
+    # 遠隔的に操作
+    @title="main_index"
+    @content="this page is"
+    @email = "fujiharaionn@mai.com"
+    erb :about
 end
