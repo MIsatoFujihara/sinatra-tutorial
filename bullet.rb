51e91de3ea5d65c3398a5ea4e06561122422f190
Misato Fujihara <fujiharaionn@gmail.com>
2017-08-23 15:42:00 +0900

    インデントの間隔を修正

--- bullet.rb
+++ bullet.rb
@@ -3,29 +3,28 @@ require 'sinatra/reloader'
 require 'active_record'
 
 ActiveRecord::Base.establish_connection(
-    "adapter" => "sqlite3",
-    "database" => "./bbs.db"
-
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
 )
 # エスケープのための決まり文句らしい
 helpers do
-    include Rack::Utils
+  include Rack::Utils
     alias_method :h, :escape_html
 end
 
 class Comment < ActiveRecord::Base
 end
-
 get '/' do
-    @comments = Comment.order("id desc").all
-    erb :index_bullet
+  @comments = Comment.order("id desc").all
+  erb :index_bullet
 end
 
 post '/new' do
-   Comment.create({:body => params[:body]})
-   redirect '/'
+  Comment.create(:body => params[:body],:author => params[:author])
+  #Comment.create({:body => params[:author]})
+  redirect '/'
 end
 
 post '/delete' do
-    Comment.find(params[:id]).destroy
+  Comment.find(params[:id]).destroy
  end
