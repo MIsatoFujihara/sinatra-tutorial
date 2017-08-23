bd7ccfbfb82e8d3160f17d3243f33c9de82af8ba
Misato Fujihara <fujiharaionn@gmail.com>
2017-08-23 10:18:53 +0900

    DBに接続

--- bullet.rb
+++ bullet.rb
@@ -1,2 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    "adapter" => "sqlite3",
+    "datebase" => "./bbs.db"
+
+)
+
+get '/' do
+    erb :index
+end
+
